b2587f4da6ff6e0efc9961b1c188e13cd414f6c9
Andrew <andrewsmith1025@gmail.com>
2021-12-03 18:19:02 -0500

    utils changes

---
+++ docs/content/docs/guides/gyroscopic_pipeline.md
@@ -0,0 +1,108 @@
+# Gyroscopic Pipeline
+We want to determine the *orientation* of a rigid body in 3 dimensions based on its *Angular Velocity*. 
+
+![fig](cube_rotation.gif)
+
+In this pipeline, we consider the rigid body's *position* to be fixed. In the future, we will consider position not to be fixed; however, we make this idealization to consider gyration first.
+
+**Before proceeding with a 3 dimensional mathematical process, let us consider an idealization in 2 dimensions.**
+
+## 2 Dimensional Idealization
+Imagine you live in _flatland_, 
+
+![fig](fig.svg)
+
+a 2-dimensional land whererin you are a 1-dimensional creature (with a rigid-ish body).
+
+![you](you.svg)
+
+You can *rotate* 20 degrees about the *origin*, labeled 'O', resulting in an *orientation* of 20 degrees counterclockwise about the origin.
+
+![you_20_degrees](you_rot_20.svg)
+
+You can also rotate 90 degrees, or pi/2 radians.
+
+![you_90_degrees](you_rot_90.svg)
+
+So, we can define your (or any rigid body's) orientation by the angle rotated minus the initial angle. 
+
+## Formalism
+Let {{< katex >}}A{{< /katex >}} be a rigid body centered at the origin in a 2 dimensional cartesian space. Define the initial orientation of the rigid body by the basis 
+{{< katex display >}}
+B
+=
+\begin{bmatrix}
+   i & j \\
+\end{bmatrix}
+=
+\begin{bmatrix}
+   1 & 0 \\
+   0 & 1
+\end{bmatrix}
+{{< /katex >}}
+shown in the following figure where *i* is red and *j* is green.
+
+![](initial.svg)
+
+The resulting *orientation* after a rotation of {{< katex >}}\theta{{< /katex >}} radians can be described by the following rotation matrix:
+
+*see page on rotation matrices*
+{{< katex display >}}
+R
+=
+\begin{bmatrix}
+   \cos(\theta) & -\sin(\theta) \\
+   \sin(\theta) & \cos(\theta)
+\end{bmatrix}.
+{{< /katex >}}
+
+We obtain the new basis by 
+
+{{< katex display >}}
+RB
+=
+\begin{bmatrix}
+   \cos(\theta) & -\sin(\theta) \\
+   \sin(\theta) & \cos(\theta)
+\end{bmatrix}
+\begin{bmatrix}
+   1 & 0 \\
+   0 & 1
+\end{bmatrix}=R.
+{{< /katex >}}
+
+## Example
+Suppose 
+{{< katex >}}\theta=\frac{\pi}{2}{{< /katex >}}
+radians. Then we have the following orientation:
+{{< katex display >}}
+RB
+=
+\begin{bmatrix}
+   \cos(\frac{\pi}{2}) & -\sin(\frac{\pi}{2}) \\
+   \sin(\frac{\pi}{2}) & \cos(\frac{\pi}{2})
+\end{bmatrix}
+\begin{bmatrix}
+   1 & 0 \\
+   0 & 1
+\end{bmatrix}=
+\begin{bmatrix}
+   \cos(\frac{\pi}{2}) & -\sin(\frac{\pi}{2}) \\
+   \sin(\frac{\pi}{2}) & \cos(\frac{\pi}{2})
+\end{bmatrix}
+{{< /katex >}}
+
+{{< katex display>}}
+=
+\begin{bmatrix}
+   0 & -1 \\
+   1 & 0
+\end{bmatrix}=
+\begin{bmatrix}
+   i & j \\
+\end{bmatrix}.
+{{< /katex >}}
+{{< columns >}}
+![gif](out.gif)
+<--->
+![gif](final.svg)
--- utils/utils.py
+++ utils/utils.py
@@ -1,21 +1,24 @@
 import numpy as np
-def get_axis_angle(dt,angular_velocity):
+
+
+def get_axis_angle(dt, angular_velocity):
     norm = np.linalg.norm(angular_velocity)
     axis = angular_velocity/norm
     angle = dt*norm
-    return axis,angle
-def plot(Q,length,save=False):
-    ## plotting
+    return axis, angle
+
+
+def plot(Q, length, save=False):
+    # plotting
     import numpy as np
     import matplotlib.pyplot as plt
     from mpl_toolkits.mplot3d import axes3d
     import matplotlib.animation as animation
     from pytransform3d import rotations as pr
 
-
     def update_lines(step, Q, rot):
         R = pr.matrix_from_quaternion(Q[step])
-        print(step,"--",Q[step])
+        print(step, "--", Q[step])
         # Draw new frame
         rot[0].set_data(np.array([0, R[0, 0]]), [0, R[1, 0]])
         rot[0].set_3d_properties([0, R[2, 0]])
@@ -37,7 +40,7 @@ def plot(Q,length,save=False):
     ax.set_ylabel("Y")
     ax.set_zlabel("Z")
 
-    R = pr.matrix_from_quaternion([1,0,0,0])
+    R = pr.matrix_from_quaternion([1, 0, 0, 0])
 
     rot = [
         ax.plot([0, 1], [0, 0], [0, 0], c="r", lw=3)[0],
@@ -52,14 +55,15 @@ def plot(Q,length,save=False):
                 c="b", lw=3, alpha=0.3)[0]
     ]
     anim = animation.FuncAnimation(fig, update_lines, length,
-                            fargs=(Q, rot),
-                            interval=1, blit=False)
+                                   fargs=(Q, rot),
+                                   interval=1, blit=False)
     if(save):
-        writer = animation.ImageMagickWriter(fps=100,bitrate=1)
-        anim.save("anim.mp4",writer=writer)
+        writer = animation.ImageMagickWriter(fps=100, bitrate=1)
+        anim.save("anim.mp4", writer=writer)
     plt.show()
 
-def preprocess_watch_data(filename,save=True,plot=True):
+
+def preprocess_watch_data(filename, save=True, plot=True):
     """
     params :
         filename : absolute or relative path to raw input file obtained from SensorLog for Apple Watch
@@ -77,57 +81,124 @@ def preprocess_watch_data(filename,save=True,plot=True):
     import pandas as pd
     df = pd.read_csv(filename)
     column_name_mapping_from_watch_names_to_my_names = {
-        'accelerometerTimestamp_sinceReboot(s)':'acc_t',
-        'accelerometerAccelerationX(G)':'acc_x',
-        'accelerometerAccelerationY(G)':'acc_y',
-        'accelerometerAccelerationZ(G)':'acc_z',
-        'motionTimestamp_sinceReboot(s)':'gyr_t',
-        'motionYaw(rad)':'yaw', 
-        'motionRoll(rad)':'roll', 
-        'motionPitch(rad)':'pitch',
-        'motionRotationRateX(rad/s)':'gyr_x', 
-        'motionRotationRateY(rad/s)':'gyr_y',
-        'motionRotationRateZ(rad/s)':'gyr_z', 
-        'motionUserAccelerationX(G)':'acc_x_2',
-        'motionUserAccelerationY(G)':'acc_y_2', 
-        'motionUserAccelerationZ(G)':'acc_z_2',
-        'motionQuaternionX(R)':'quat_x',
-        'motionQuaternionY(R)':'quat_y', 
-        'motionQuaternionZ(R)':'quat_z', 
-        'motionQuaternionW(R)':'quat_w'
+        'accelerometerTimestamp_sinceReboot(s)': 'acc_t',
+        'accelerometerAccelerationX(G)': 'acc_x',
+        'accelerometerAccelerationY(G)': 'acc_y',
+        'accelerometerAccelerationZ(G)': 'acc_z',
+        'motionTimestamp_sinceReboot(s)': 'gyr_t',
+        'motionYaw(rad)': 'yaw',
+        'motionRoll(rad)': 'roll',
+        'motionPitch(rad)': 'pitch',
+        'motionRotationRateX(rad/s)': 'gyr_x',
+        'motionRotationRateY(rad/s)': 'gyr_y',
+        'motionRotationRateZ(rad/s)': 'gyr_z',
+        'motionUserAccelerationX(G)': 'acc_x_2',
+        'motionUserAccelerationY(G)': 'acc_y_2',
+        'motionUserAccelerationZ(G)': 'acc_z_2',
+        'motionQuaternionX(R)': 'quat_x',
+        'motionQuaternionY(R)': 'quat_y',
+        'motionQuaternionZ(R)': 'quat_z',
+        'motionQuaternionW(R)': 'quat_w'
     }
     df = df[list(column_name_mapping_from_watch_names_to_my_names.keys())]
-    df = df.rename(column_name_mapping_from_watch_names_to_my_names,axis=1)
+    df = df.rename(column_name_mapping_from_watch_names_to_my_names, axis=1)
     if(save):
-        df.to_csv(filename.replace(".csv","_preprocessed.csv"),index=False)
+        df.to_csv(filename.replace(".csv", "_preprocessed.csv"), index=False)
     if(plot):
         import matplotlib.pyplot as plt
-        fig, (ax1,ax2,ax3) = plt.subplots(3,sharex=True,sharey=True)
+        fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=True)
         import seaborn as sns
-        fig.set_size_inches(8.5,11)
-        sns.lineplot(ax=ax1,data=df,x='gyr_t',y='gyr_x')
-        sns.lineplot(ax=ax2,data=df,x='gyr_t',y='gyr_y')
-        sns.lineplot(ax=ax3,data=df,x='gyr_t',y='gyr_z')
+        fig.set_size_inches(8.5, 11)
+        sns.lineplot(ax=ax1, data=df, x='gyr_t', y='gyr_x')
+        sns.lineplot(ax=ax2, data=df, x='gyr_t', y='gyr_y')
+        sns.lineplot(ax=ax3, data=df, x='gyr_t', y='gyr_z')
 
-        fig, (ax1,ax2,ax3) = plt.subplots(3,sharex=True,sharey=True)
-        fig.set_size_inches(8.5,11)
-        sns.histplot(ax=ax1,data=df,x="gyr_x")
-        sns.histplot(ax=ax2,data=df,x="gyr_y")
-        sns.histplot(ax=ax3,data=df,x="gyr_z")
+        fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=True)
+        fig.set_size_inches(8.5, 11)
+        sns.histplot(ax=ax1, data=df, x="gyr_x")
+        sns.histplot(ax=ax2, data=df, x="gyr_y")
+        sns.histplot(ax=ax3, data=df, x="gyr_z")
     return df
 
-def make_cartesian_axes():
+
+def save_fig_as_pgf():
+    import matplotlib
     import matplotlib.pyplot as plt
-    # Select length of axes and the space between tick labels
+    matplotlib.use("pgf")
+    plt.style.use("../utils/style.txt")
+    matplotlib.rcParams.update({
+        "pgf.texsystem": "xelatex",
+        'font.family': 'serif',
+        'text.usetex': True,
+        'pgf.rcfonts': False
+    })
+    plt.savefig("fig.pgf")
+
+
+def get_stick_figure_patch(ax=None, angle=0):
+    from matplotlib.path import Path
+    from matplotlib.patches import PathPatch
+    from matplotlib.transforms import Affine2D
+    verts = [
+        (-2., -2.),
+        (-1., -2.),
+        (-.5, -1),
+        (.5, -1),
+        (1, -2),
+        (2, -2),
+        (1, 0),
+        (2, 0),
+        (2, 1),
+        (.5, 1),
+        (.5, 2.),
+        (-.5, 2),
+        (-.5, 1),
+        (-2, 1),
+        (-2, 0),
+        (-1, 0),
+        (0., 0.),  # ignored
+    ]
+
+    codes = [
+        Path.MOVETO,
+        Path.LINETO,
+        Path.LINETO,
+        Path.LINETO,
+        Path.LINETO,
+        Path.LINETO,
+        Path.LINETO,
+        Path.LINETO,
+        Path.LINETO,
+        Path.LINETO,
+        Path.LINETO,
+        Path.LINETO,
+        Path.LINETO,
+        Path.LINETO,
+        Path.LINETO,
+        Path.LINETO,
+        Path.CLOSEPOLY,
+    ]
+
+    path = Path(verts, codes)
+
+    patch = PathPatch(path, facecolor=(0, .3, .5, .3), lw=2)
+    from math import pi
+    angle = (angle*180)/pi
+    t2 = Affine2D().rotate_deg(angle) + ax.transData
+
+    patch.set_transform(t2)
+    ax.add_patch(patch)
+    return patch
+
+
+def get_2d_cartesian_axes():
+    import matplotlib.pyplot as plt
+    import numpy as np
+
     xmin, xmax, ymin, ymax = -5, 5, -5, 5
     ticks_frequency = 1
-
-    # Plot points
     plt.figure(1)
-    # ax = plt.plot(figsize=(10, 10))
-
     fig, ax = plt.subplots(figsize=(5, 5))
-    
     fig.patch.set_facecolor('#ffffff')
     ax.set(xlim=(xmin-1, xmax+1), ylim=(ymin-1, ymax+1), aspect='equal')
     ax.spines['bottom'].set_position('zero')
@@ -137,12 +208,12 @@ def make_cartesian_axes():
     ax.set_xlabel('$x$', size=14, labelpad=-24, x=1.02)
     ax.set_ylabel('$y$', size=14, labelpad=-21, y=1.02, rotation=0)
     plt.text(0.49, 0.49, r"$O$", ha='right', va='top',
-        transform=ax.transAxes,
-            horizontalalignment='center', fontsize=14)
+             transform=ax.transAxes,
+             horizontalalignment='center', fontsize=14)
     x_ticks = np.arange(xmin, xmax+1, ticks_frequency)
     y_ticks = np.arange(ymin, ymax+1, ticks_frequency)
     ax.set_xticks(ticks=[])
     ax.set_yticks(ticks=[])
     ax.set_xticks(np.arange(xmin, xmax+1), minor=True)
     ax.set_yticks(np.arange(ymin, ymax+1), minor=True)
-    return fig,ax
+    return fig, ax
