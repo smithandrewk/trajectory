0a987327da4389dbacc9ebd828954b42b4ae1f27
Andrew <andrewsmith1025@gmail.com>
2022-01-27 08:54:12 -0500

    refactor and finish tcp server

--- pain.py
+++ pain.py
@@ -1,50 +1,54 @@
 #!/usr/bin/env python3
+import math  # for sine and cosine functions
+from matplotlib import pyplot as plt, table
+from math import sin
+import random
+import cv2
 import time
+import matplotlib
+import numpy as np
+
+
 def follow(thefile):
-    thefile.seek(0,2) # Go to the end of the file
+    count = 0
+    thefile.seek(0, 2)  # Go to the end of the file
     while True:
+        count +=1
+        if(count>1000):
+            thefile.seek(0, 2)  # Go to the end of the file
+            count = 0
+        # thefile.seek(0, 2)  # Go to the end of the file
         line = thefile.readline()
         if not line:
-            time.sleep(0.001) # Sleep briefly
+            print("noline")
+            time.sleep(0.01)  # Sleep briefly
             continue
         yield line
-"""
-loggingTime(txt),
-loggingSample(N),
-accelerometerTimestamp_sinceReboot(s),
-accel    erometerAccelerationX(G),
-accelerometerAccelerationY(G),
-accelerometerAccelerat    ionZ(G),
-gyroTimestamp_sinceReboot(s),
-gyroRotationX(rad/s),
-gyroRotationY(rad/s    ),
-gyroRotationZ(rad/s),
-magnetometerTimestamp_sinceReboot(s),
-magnetometerX(µT)    ,
-magnetometerY(µT),
-magnetometerZ(µT)
-"""
+
+
 def process_line(line):
+    """
+    loggingTime(txt),
+    loggingSample(N),
+    accelerometerTimestamp_sinceReboot(s),
+    accelerometerAccelerationX(G),
+    accelerometerAccelerationY(G),
+    accelerometerAccelerationZ(G),
+    gyroTimestamp_sinceReboot(s),
+    gyroRotationX(rad/s),
+    gyroRotationY(rad/s),
+    gyroRotationZ(rad/s),
+    magnetometerTimestamp_sinceReboot(s),
+    magnetometerX(µT),
+    magnetometerY(µT),
+    magnetometerZ(µT)
+    """
     line = line.strip()
     line = line.split(',')
     t = line[2]
     acc = line[3:6]
-    return t,acc
-from matplotlib import pyplot as plt
+    return t, acc
 
-
-fig = plt.figure(figsize=(10, 20), dpi=200)
-ax = fig.add_subplot(111)
-ax.set_ylim([-6, 6])
-plt.ion()
-plt.show(block=False)
-
-x = []
-import numpy as np
-import matplotlib
-acc_x = []
-acc_y = []
-acc_z = []
 
 def mypause(interval):
     backend = plt.rcParams['backend']
@@ -57,33 +61,66 @@ def mypause(interval):
             canvas.start_event_loop(interval)
             return
 
-f = open('stream','r')
+
+# fig = plt.figure(figsize=(10, 20), dpi=100)
+# ax = fig.add_subplot(111)
+# ax.set_ylim([-6, 6])
+# plt.ion()
+# plt.show(block=False)
+x = []
+acc_x = []
+acc_y = []
+acc_z = []
+last_t = -1
+
+
+# Plot values in opencv program
+
+# Plot values in opencv program
+class Plotter:
+    def __init__(self, plot_width, plot_height, num_plot_values):
+        self.width = plot_width
+        self.height = plot_height
+        self.color_list = [(255, 0, 0), (0, 250, 0), (0, 0, 250),
+                           (0, 255, 250), (250, 0, 250), (250, 250, 0),
+                           (200, 100, 200), (100, 200, 200), (200, 200, 100)]
+        self.color = []
+        self.val = []
+        self.plot = np.ones((self.height, self.width, 3))*255
+
+        for i in range(num_plot_values):
+            self.color.append(self.color_list[i])
+
+    # Update new values in plot
+    def multiplot(self, val, label="plot"):
+        self.val.append(val)
+        while len(self.val) > self.width:
+            self.val.pop(0)
+
+        self.show_plot(label)
+
+    # Show plot using opencv imshow
+    def show_plot(self, label):
+        self.plot = np.ones((self.height, self.width, 3))*255
+        # cv2.line(self.plot, (0, int(self.height/2) ), (self.width, int(self.height/2)), (0,255,0), 1)
+        for i in range(len(self.val)-1):
+            for j in range(len(self.val[0])):
+                cv2.line(self.plot, (i, int(self.height/2) - self.val[i][j]), (i+1, int(
+                    self.height/2) - self.val[i+1][j]), self.color[j], 1)
+
+        cv2.imshow(label, self.plot)
+        cv2.waitKey(10)
+
+
+p = Plotter(400, 1000, 3)
+
+f = open('stream', 'r')
 for line in follow(f):
-    if(len(x)==300):
-        del x[0]
-        del acc_x[0]
-        del acc_y[0]
-        del acc_z[0]
-
-
-        # x.pop()
-        # acc_x.pop()
-        # acc_y.pop()
-        # acc_z.pop()
-    t,acc = process_line(line)
-    print(t,acc)
-    print("Length : ",len(x))
-    acc = list(map(float,acc))
-    print(acc)
-    x.append(float(t))
-    acc_x.append(acc[0])
-    acc_y.append(acc[1])
-    acc_z.append(acc[2])
-    plt.autoscale(True)
-    plt.clf()
-
-    plt.plot(x,acc_x,'r',markersize=1,linewidth=.5)
-    plt.plot(x,acc_y,'g',markersize=1,linewidth=.5)
-    plt.plot(x,acc_z,'b',markersize=1,linewidth=.5)
-    # plt.pause(0.0001) 
-    mypause(.0001)        
+    t, acc = process_line(line)
+    t = float(t)
+    acc = list(map(float, acc))
+    delta_t = t-last_t
+    Hz = 1/delta_t
+    # print(t, delta_t, Hz)
+    last_t = t
+    p.multiplot([int(acc[0]*200), int(acc[1]*200), int(acc[2]*200)])
